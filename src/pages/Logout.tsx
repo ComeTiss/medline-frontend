386e69f6b3d7edcf22e91b6b0cb5fb4f0f460e86
ComeTiss <cometisserand@rocketmail.com>
2020-04-17 14:59:06 +0200

    [minor] few minor fixes

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 function Logout() {
+  const [, setCookies] = useCookies();
+  setCookies("access_token", null);
   return <Redirect to="/login" />;
 }
 
