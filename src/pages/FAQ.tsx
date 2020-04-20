bdc7af0f655e1ef829a8a1be0ab8eb21b76d9c01
Jason <jasylwong@gmail.com>
2020-04-20 20:56:21 +0100

    add questions text

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -8,6 +8,13 @@ function FAQ() {
     <div>
       <NavBar />
       <h2>FAQ</h2>
+      Can I buy medical supplies from Medline.io?
+      Then how does MedLine.io make money?
+      How do you prioritise "Needs"?
+      What are "Leads"?
+      Are the Leads / Suppliers certified?
+      Who's using the platform?
+      Are you related to MedlinePlus or Medline Industries?
     </div>
   );
 }
