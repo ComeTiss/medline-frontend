b71990e12d6bfc4127b1b48146fabe464aa1cf2a
Jason <jasylwong@gmail.com>
2020-04-21 21:08:08 +0100

    add background image to files

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -24,7 +24,7 @@ const useStyles = makeStyles((theme: Theme) =>
       color: "#233768",
       fontWeight: "bolder",
       textAlign: 'center',
-      padding: '20px',
+      padding: '40px',
       fontSize: '20px',
       fontFamily: 'Verdana'
     },
@@ -139,54 +139,105 @@ export default function FAQ() {
           </ExpansionPanelDetails>
         </ExpansionPanel>
         <hr />
+        <ExpansionPanel className={classes.expansionPanel}>
+          <ExpansionPanelSummary
+            expandIcon={<AddIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography className={classes.heading}>What are "Leads"?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.answer}>
+              Leads can be any individual or organization that has medical supplies in stock and available to ship.
+              These can include personal volunteers, NGOs, distributors and distribution businesses, as well as 
+              manufacturers.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <hr />
+        <ExpansionPanel className={classes.expansionPanel}>
+          <ExpansionPanelSummary
+            expandIcon={<AddIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography className={classes.heading}>Are the Leads / Suppliers certified?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.answer}>
+              <p>
+                Unfortunately, no; since Leads can also be individual volunteers, the platform is not able to certify
+                Leads stocks at this time. However, the Leads list has highlighted suppliers that have already
+                effectively shipped to another member on the platform. These shipments have been verified by recipients
+                and the Leads are marked with a green flag.
+              </p>
+              <p>
+                In addition, we're working on enabling Leads to upload photos of their products, stocks, certifications
+                and licenses, etc. But these cannot be verified by platform volunteers at this time.
+              </p>
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <hr />
+        <ExpansionPanel className={classes.expansionPanel}>
+          <ExpansionPanelSummary
+            expandIcon={<AddIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography className={classes.heading}>Who's using the platform?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.answer}>
+              <p>Our community members consist of:</p>
+              <p>
+                Medical Personnel<br/>
+                Healthcare Organisations<br/>
+                Volunteers / NGOs<br/>
+                Supply Distributors<br/>
+                Supply Manufacturers
+              </p>
+              <p>If you know anyone that may benefit from this site. Please share with them.</p>
+              <p>MedLine.io is 100% free to use and takes just a few minutes to post Needs / Leads!</p>
+              <p>Here's our [link] and [social accounts].</p>
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <hr />
+        <ExpansionPanel className={classes.expansionPanel}>
+          <ExpansionPanelSummary
+            expandIcon={<AddIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography className={classes.heading}>Are you related to MedlinePlus or Medline Industries?</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.answer}>
+              <p>
+                Apologies for any confusion. MedLine.io is not related in any way to MedlinePlus or 
+                Medline Industries. We don’t know them, and they don't know us (at least not yet).
+              </p>
+              <p>In case you're wondering, according to their websites:</p>
+              <p>
+                MedlinePlus is “an online health information resource” and “service of the National 
+                Library of Medicine (NLM), the world’s largest medical library, which is part of the National
+                Institues of Health (NIH).
+                You can visit them at https://medlineplus.gov/
+              </p>
+              <p>
+                Medline Industries is “the largest privately held manufacturer and distributor of medical supplies.” 
+                You can vist them at https://www.medline.com/
+              </p>
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <hr />
       </div>
     </div>
   );
 }
 
-// What are "Leads"?
-// Leads can be any individual or organization that has medical supplies in stock and available to ship.
-// These can include personal volunteers, NGOs, distributors and distribution businesses, as well as 
-// manufacturers.
 
-
-
-// Are the Leads / Suppliers certified?
-// Unfortunately, no; since Leads can also be individual volunteers, the platform is not able to certify
-// Leads stocks at this time. However, the Leads list has highlighted suppliers that have already
-// effectively shipped to another member on the platform. These shipments have been verified by recipients
-// and the Leads are marked with a green flag.
-
-// In addition, we're working on enabling Leads to upload photos of their products, stocks, certifications
-// and licenses, etc. But these cannot be verified by platform volunteers at this time.
-
-
-// Who's using the platform?
-// Our community members consist of:
-// Medical Personnel
-// Healthcare Organisations
-// Volunteers / NGOs
-// Supply Distributors
-// Supply Manufacturers
-
-// If you know anyone that may benefit from this site. Please share with them.
-
-// MedLine.io is 100% free to use and takes just a few minutes to post Needs / Leads!
-
-// Here's our [link] and [social accounts].
-
-
-// Are you related to MedlinePlus or Medline Industries?
-// Apologies for any confusion. MedLine.io is not related in any way to MedlinePlus or 
-// Medline Industries. We don’t know them, and they don't know us (at least not yet).
-
-// In case you're wondering, according to their websites:
-
-// MedlinePlus is “an online health information resource” and “service of the National 
-// Library of Medicine (NLM), the world’s largest medical library, which is part of the National
-// Institues of Health (NIH).
-// You can visit them at https://medlineplus.gov/
-
-// Medline Industries is “the largest privately held manufacturer and distributor of medical supplies.” 
-// You can vist them at https://www.medline.com/
 
