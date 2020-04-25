829506214626ef25dd0fa5b37c408b33d434a3b9
Jason <jasylwong@gmail.com>
2020-04-25 21:08:44 +0100

    add outro text

--- src/pages/JoinUs.tsx
+++ src/pages/JoinUs.tsx
@@ -6,9 +6,14 @@ const logo = require("../media/join_us_background.jpg");
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
-      width: "58%",
+      width: "80%",
       position: "absolute",
-      left: "21%"
+      left: "10%"
+    },
+    background: {
+      position: "absolute",
+      float: "left",
+      zIndex: -1
     },
     title: {
       color: "#233768",
@@ -18,14 +23,22 @@ const useStyles = makeStyles(() =>
       fontSize: "20px",
       fontFamily: "Verdana"
     },
-    background: {
-      position: "absolute",
-      float: "left",
-      opacity: "20%"
+    intro_text: {
+      color: "#5b5b5b",
+      fontWeight: "bolder",
+      textAlign: "center",
+      fontSize: "18px",
+      fontFamily: "Verdana"
+    },
+    outro_text: {
+      color: "white",
+      fontWeight: "bolder",
+      textAlign: "center",
+      fontSize: "18px",
+      fontFamily: "Verdana"
     }
   })
 );
-
 
 export default function JoinUs() {
   const classes = useStyles();
@@ -34,14 +47,30 @@ export default function JoinUs() {
     <>
       <NavBar />
       <img
-        src={ String(logo) }
+        src={String(logo)}
         style={{ width: "100%" }}
-        className={ classes.background }
+        className={classes.background}
         alt="latex_gloves"
       />
       <div className={classes.title}>Join Us</div>
       <div className={classes.root}>
-        
+        <div className={classes.intro_text}>
+          Would you like to join our international team of talented volunteers
+          developing, operating and optimising&nbsp;
+          <span style={{ color: "#233768" }}>Medline.io</span> in our ongoing
+          efforts to support front-line medical personnel fighting against
+          COVID-19?
+          <br />
+          Your help is greatly appreciated through:
+        </div>
+
+        <div className={classes.outro_text}>
+          If you are, or know of credible transport, customs, quality control or
+          other service providers that can facilitate efficient and effective
+          delivery of medical supplies in the best interest of all parties,
+          please contact us. We are currently building a database and will make
+          it available to the public soon.
+        </div>
       </div>
     </>
   );
