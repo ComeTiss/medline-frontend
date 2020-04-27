ea6c7b51f53a360d52ecd1fff9d86735fd916496
Liam Scott <liam@Liams-MBP.lan>
2020-04-27 13:53:28 +0100

    flex box grid structure added

--- src/components/homepage/ProgressTracker.tsx
+++ src/components/homepage/ProgressTracker.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import { makeStyles } from '@material-ui/core'
+const useStyles = makeStyles( {
+    progressTracker: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    stepLevel: {
+        display: "flex",
+    },
+    step: {
+        width: "500px"
+    },
+} )
 const ProgressTracker = () => {
+    const styles = useStyles();
     return (
-        <p>progress tracker</p>
+        <div className={styles.progressTracker}>
+            <div className={styles.stepLevel}>
+                <div className={styles.step}></div>
+                <div className={styles.step}></div>
+                <div className={styles.step}></div>
+            </div>
+            <div className={styles.stepLevel}>
+                <div className={styles.step}></div>
+                <div className={styles.step}></div>
+                <div className={styles.step}></div>
+            </div>
+            <div className={styles.stepLevel}>
+                <div className={styles.step}></div>
+                <div className={styles.step}></div>
+                <div className={styles.step}></div>
+            </div>
+        </div>
     )
 }
 
