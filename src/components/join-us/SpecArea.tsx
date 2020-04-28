03a6e23d2327080fca4402881cc62929091857e5
Jason <jasylwong@gmail.com>
2020-04-28 01:54:34 +0100

    align tech and design

--- src/components/join-us/SpecArea.tsx
+++ src/components/join-us/SpecArea.tsx
@@ -9,6 +9,13 @@ type Props = {
 
 const useStyles = makeStyles(() =>
   createStyles({
+    container: {
+      background: "rgba(255, 255, 255, 0.6)",
+      padding: "10px",
+      margin: "20px",
+      width: "43%",
+      display: "inline-block"
+    },
     title: {
       color: "#233768",
       fontWeight: "bolder",
@@ -20,6 +27,12 @@ const useStyles = makeStyles(() =>
       color: "#5b5b5b",
       fontSize: "15px",
       fontFamily: "Verdana"
+    },
+    icon: {
+      width: "12%",
+    },
+    iconPara: {
+      textAlign: "center"
     }
   })
 );
@@ -29,10 +42,12 @@ export default function SpecArea(props: Props) {
   const { icon, title, content } = props;
 
   return (
-    <div>
-      <img src={icon} style={{ width: "5%" }} alt={title} />
+    <div className={classes.container}>
+      <p className={classes.iconPara}><img src={icon} className={classes.icon} alt={title} /></p>
       <div className={classes.title}>{title}</div>
       <div className={classes.content}>{content}</div>
     </div>
   );
 }
+
+// style={{ width: "5%" }}
--- src/pages/JoinUs.tsx
+++ src/pages/JoinUs.tsx
@@ -8,19 +8,14 @@ const designIcon = require("../media/join_us_images/design-icon.svg");
 const mediaIcon = require("../media/join_us_images/media-icon.svg");
 const advisoryIcon = require("../media/join_us_images/advisory-icon.svg");
 const projectManagementIcon = require("../media/join_us_images/project-management-icon.svg");
-const othersIcon = require("../media/join_us_images/others-icon.svg");
+const othersIcon = require("../media/join_us_images/others-icon2.svg");
 
 const useStyles = makeStyles(() =>
   createStyles({
-    root: {
-      width: "80%",
-      position: "absolute",
-      left: "10%"
-    },
-    background: {
-      position: "absolute",
-      float: "left",
-      zIndex: -1
+    overallContainer: {
+      background: `url(${backgroundImage}) no-repeat center center`,
+      backgroundSize: "cover",
+      backgroundPosition: "0 -100px"
     },
     title: {
       color: "#233768",
@@ -35,14 +30,28 @@ const useStyles = makeStyles(() =>
       fontWeight: "bolder",
       textAlign: "center",
       fontSize: "18px",
-      fontFamily: "Verdana"
+      fontFamily: "Verdana",
+      width: "80%",
+      marginLeft: "10%"
     },
+    // root: {
+    //   width: "80%",
+    //   position: "absolute",
+    //   left: "10%"
+    // },
+    // background: {
+    //   position: "absolute",
+    //   float: "left",
+    //   zIndex: -1
+    // },
     outro_text: {
       color: "white",
       fontWeight: "bolder",
       textAlign: "center",
       fontSize: "18px",
-      fontFamily: "Verdana"
+      fontFamily: "Verdana",
+      width: "80%",
+      marginLeft: "10%"
     },
     thanks_text: {
       color: "white",
@@ -56,7 +65,17 @@ const useStyles = makeStyles(() =>
       textAlign: "center",
       fontSize: "12px",
       fontFamily: "Verdana"
-    }
+    },
+    left_margin: {
+      // marginLeft: "1%",
+      // display: "inline-block",
+      // float: "left"
+    },
+    grid_container: {
+      display: "grid",
+      gridTemplateColumns: "auto auto"
+    },
+
   })
 );
 
@@ -64,16 +83,15 @@ export default function JoinUs() {
   const classes = useStyles();
 
   return (
-    <>
+    <div className={classes.overallContainer}>
       <NavBar />
-      <img
+      {/* <img
         src={String(backgroundImage)}
         style={{ width: "100%" }}
         className={classes.background}
         alt="latex_gloves"
-      />
+      /> */}
       <div className={classes.title}>Join Us</div>
-      <div className={classes.root}>
         <div className={classes.intro_text}>
           Would you like to join our international team of talented volunteers
           developing, operating and optimising&nbsp;
@@ -83,152 +101,180 @@ export default function JoinUs() {
           <br />
           Your help is greatly appreciated through:
         </div>
-        <SpecArea
-          icon={String(techIcon)}
-          title="TECH"
-          content={
-            <div>
-              <ul>
-                <li>
-                  Full-stack Developers: Ongoing builds. Continuous improvements
-                  to help users more, better, faster.
-                </li>
-                <li>
-                  Back-End: Site maintenance and security. Keeping the platform
-                  up and running smoothly. Protecting privacy of our members.
-                </li>
-                <li>
-                  Data Scientists/Math Whizzes: Developing algorithms to
-                  optimise forecast and prioritisation of &apos;Needs&apos;
-                  urgencies. We have to clearly identify and get them helped
-                  first and foremost.
-                </li>
-              </ul>
-            </div>
-          }
-        />
-        <SpecArea
-          icon={String(designIcon)}
-          title="DESIGN"
-          content={
-            <div>
-              <ul>
-                <li>
-                  Product Designers: User insights, concepting ideas through
-                  design-thinking. Inspired innovation that works.
-                </li>
-                <li>
-                  UI/UX Designers: User-friendly, intuitive, easy and quick to
-                  navigate. Our members are very busy people.
-                </li>
-                <li>
-                  Visual Designers: Making things pretty on the platform and
-                  across our social channels. Cause if we can, then why not.
-                </li>
-              </ul>
-            </div>
-          }
-        />
-        <SpecArea
-          icon={String(mediaIcon)}
-          title="MEDIA / CONTENT"
-          content={
-            <div>
-              <ul>
-                <li>
-                  Writers, Bloggers, Copywriters: Effectively communicating
-                  thoughts, ideas, and inspirations through the written word.
-                  Across teams, platforms and social media.
-                </li>
-                <li>
-                  Community Managers: Share Medline.io to medics and suppliers
-                  that could benefit from this platform. Operating our social
-                  accounts to keep in touch.
-                </li>
-                <li>
-                  Translators: Any and every human language. Viruses know no
-                  borders. If we can help share needed info across global
-                  communities, then we should get to it.
-                </li>
-                <li>
-                  Marketing/PR/Communications Practitioners: Get the word out
-                  across media, with targeted editorials, verticals and
-                  channels. Let relevant folks know that Medline.io is working
-                  to help out the best we can, as much as we can.
-                </li>
-                <li>
-                  Customer Service Representatives: Answering emails and
-                  corresponding with platform members that have user experience
-                  issues, questions or comments.
-                </li>
-                <li>
-                  Crowdfunding Experts: Sharing effective fundraising best
-                  practices via GoFundMe, Kickstarter, Indiegogo, etc. Our
-                  volunteers have donated innumerable hours building and
-                  operating this site. Hate to have them pay for it too.
-                </li>
-              </ul>
-            </div>
-          }
-        />
-        <SpecArea
-          icon={String(advisoryIcon)}
-          title="ADVISORY"
-          content={
-            <div>
-              <ul>
-                <li>
-                  Healthcare Professionals: Providing insights and expertise on
-                  &apos;Needs&apos; requirements, any important info we should
-                  consider, or challenges when procuring supplies. Letting us
-                  know what we haven&apos;t thought of yet.
-                </li>
-                <li>
-                  Pandemic Experts: Sharing insights and timely forecasting of
-                  viral spread to help with urgency/prioritisation calculations.
-                </li>
-                <li>
-                  Legal Experts: Trademarks, intellectual properties, adhering
-                  to GDPR etc. Who doesn&apos;t need a lawyer?
-                </li>
-              </ul>
-            </div>
-          }
-        />
-        <SpecArea
-          icon={String(projectManagementIcon)}
-          title="PROJECT MANAGEMENT"
-          content={
-            <div>
-              <ul>
-                <li>
-                  Project Managers: Taking update and expansion initiatives from
+        <br />
+        <div style={{ display: "inline-block" }}>
+          <SpecArea
+            icon={String(techIcon)}
+            title="TECH"
+            content={
+              <div>
+                <ul>
+                  <li>
+                    <strong>Full-stack Developers:</strong> Ongoing builds. Continuous improvements
+                    to help users more, better, faster.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>Back-End:</strong> Site maintenance and security. Keeping the platform
+                    up and running smoothly. Protecting privacy of our members.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>Data Scientists/Math Whizzes:</strong> Developing algorithms to
+                    optimise forecast and prioritisation of &apos;Needs&apos;
+                    urgencies. We have to clearly identify and get them helped
+                    first and foremost.
+                  </li>
+                </ul>
+              </div>
+            }
+          />
+
+          <SpecArea
+            icon={String(designIcon)}
+            title="DESIGN"
+            content={
+              <div>
+                <ul>
+                  <li>
+                    <strong>Product Designers:</strong> User insights, concepting ideas through
+                    design-thinking. Inspired innovation that works.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>UI/UX Designers:</strong> User-friendly, intuitive, easy and quick to
+                    navigate. Our members are very busy people.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>Visual Designers:</strong> Making things pretty on the platform and
+                    across our social channels. Cause if we can, then why not.
+                  </li>
+                  <br />
+                </ul>
+              </div>
+            }
+          />         
+        </div>
+        <div className={classes.left_margin}>
+          <SpecArea
+            icon={String(mediaIcon)}
+            title="MEDIA / CONTENT"
+            content={
+              <div>
+                <ul>
+                  <li>
+                    <strong>Writers, Bloggers, Copywriters:</strong> Effectively communicating
+                    thoughts, ideas, and inspirations through the written word.
+                    Across teams, platforms and social media.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>Community Managers:</strong> Share Medline.io to medics and suppliers
+                    that could benefit from this platform. Operating our social
+                    accounts to keep in touch.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>Translators:</strong> Any and every human language. Viruses know no
+                    borders. If we can help share needed info across global
+                    communities, then we should get to it.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>Marketing/PR/Communications Practitioners:</strong> Get the word out
+                    across media, with targeted editorials, verticals and
+                    channels. Let relevant folks know that Medline.io is working
+                    to help out the best we can, as much as we can.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>Customer Service Representatives:</strong> Answering emails and
+                    corresponding with platform members that have user experience
+                    issues, questions or comments.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>Crowdfunding Experts:</strong> Sharing effective fundraising best
+                    practices via GoFundMe, Kickstarter, Indiegogo, etc. Our
+                    volunteers have donated innumerable hours building and
+                    operating this site. Hate to have them pay for it too.
+                  </li>
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                  <br />
+                </ul>
+              </div>
+            }
+          />  
+          <SpecArea
+            icon={String(advisoryIcon)}
+            title="ADVISORY"
+            content={
+              <div>
+                <ul>
+                  <li>
+                    <strong>Healthcare Professionals:</strong> Providing insights and expertise on
+                    &apos;Needs&apos; requirements, any important info we should
+                    consider, or challenges when procuring supplies. Letting us
+                    know what we haven&apos;t thought of yet.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>Pandemic Experts:</strong> Sharing insights and timely forecasting of
+                    viral spread to help with urgency/prioritisation calculations.
+                  </li>
+                  <br />
+                  <li>
+                    <strong>Legal Experts:</strong> Trademarks, intellectual properties, adhering
+                    to GDPR etc. Who doesn&apos;t need a lawyer?
+                  </li>
+                </ul>
+              </div>
+            }
+          />
+          
+          <SpecArea
+            icon={String(projectManagementIcon)}
+            title="PROJECT MANAGEMENT"
+            content={
+              <div>
+                <ul>
+                  <strong>Project Managers:</strong> Taking update and expansion initiatives from
                   zero to hero. The glue that connects people together and gets
                   things happening.
-                </li>
-              </ul>
-            </div>
-          }
-        />
-        <SpecArea
-          icon={String(othersIcon)}
-          title="OTHERS"
-          content={
-            <div>
-              <ul>
-                <li>
+                </ul>
+              </div>
+            }
+          />
+          {/* <SpecArea
+            icon={String(othersIcon)}
+            title="OTHERS"
+            content={
+              <div>
+                <ul>
                   Have a skill, talent, or expertise you feel might be relevant
-                  to operating, building or improving the platform? Reach out to
-                  &nbsp;
+                  to operating, building or improving the platform? Reach out 
+                  to&nbsp;
                   <a href="mailto:ssupport@medline.io" target="_top">
                     support@medline.io
                   </a>
                   &nbsp;and let us know!
-                </li>
-              </ul>
-            </div>
-          }
-        />
+                </ul>
+              </div>
+            }
+          /> */}
+        </div>
+        <br />
         <div className={classes.outro_text}>
           If you are, or know of credible transport, customs, quality control or
           other service providers that can facilitate efficient and effective
@@ -252,7 +298,6 @@ export default function JoinUs() {
           <br />
           Project Management:
         </div>
-      </div>
-    </>
+    </div>
   );
 }
