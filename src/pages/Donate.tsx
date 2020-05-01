64c4e9d1bf76f0c2e927f2891fe098c6cf388d79
Frog <shmily40686@gmail.com>
2020-05-01 13:08:35 -0700

    Fix import on Donat epage

--- src/pages/Donate.tsx
+++ src/pages/Donate.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "../components/navigation/NavBar";
-import Footer from "../pages/Footer";
+import Footer from "../components/footer/Footer";
 import {
   makeStyles,
   Typography,
