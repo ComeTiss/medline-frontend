dd15bd0c96f6487080fbbbfc407f1ebe2a1916c3
Frog <shmily40686@gmail.com>
2020-05-03 17:16:10 -0700

    set up the background for needs page

--- src/components/needLeadViewPage/needsView.tsx
+++ src/components/needLeadViewPage/needsView.tsx
@@ -6,6 +6,8 @@ import { GET_NEEDS } from "../../service/apollo/queries";
 import Need from "../../service/models/need.model";
 import SearchIcon from '@material-ui/icons/Search';
 
+import maskImage from "../../images/face-masks-on-blue-background-3786155.jpg";
+
 
 
 
@@ -14,10 +16,21 @@ function NeedsView() {
     const [page, setPage] = useState(1);
 
     const useStyles = makeStyles(() => ({
+        needContainer: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            background: `url(${maskImage}) no-repeat center center`,
+            backgroundSize: "cover",
+            height: "100vh"
+        },
         boxNeed: {
             display: "flex",
             flexDirection: "column",
-            background: "white"
+            background: "white",
+            width: "80%",
+            height: "67vh",
+            padding: "35px"
         },
         needHeader: {
             display: "flex",
@@ -31,7 +44,9 @@ function NeedsView() {
             justifyContent: "space-between",
             alignItems: "center",
             border: "1px solid grey",
-            background: "white"
+            background: "white",
+            paddingRight: "2px",
+            marginRight: "0px"
         },
         input: {
             width: "100%",
@@ -40,7 +55,7 @@ function NeedsView() {
             outline: "none"
         },
         table: {
-
+            border: "1px solid black"
         },
         header: {
 
@@ -88,29 +103,31 @@ function NeedsView() {
     );
 
     return (
-        <Box className={styles.boxNeed}>
-            <Box className={styles.needHeader}>
-                <h1>Needs</h1>
-                <Container className={styles.inputBox}>
-                    <input className={styles.input} type="text" />
-                    <SearchIcon/>
-                </Container>
+        <Box className={styles.needContainer} >
+            <Box className={styles.boxNeed}>
+                <Box className={styles.needHeader}>
+                    <h1>Needs</h1>
+                    <Container className={styles.inputBox}>
+                        <input className={styles.input} type="text" />
+                        <SearchIcon/>
+                    </Container>
+                </Box>
+                <table className={styles.table}>
+                    <thead>
+                        <tr>
+                            <th className={styles.header}>URGENCY</th>
+                            <th className={styles.header}>ITEMS</th>
+                            <th className={styles.header}>POSTED BY</th>
+                            <th className={styles.header}>COUNTRY</th>
+                            <th className={styles.header}>CITY</th>
+                            <th className={styles.header}>QTY</th>
+                            <th className={styles.header}>DATE</th>
+                            <th className={styles.header}>BUDGET</th>
+                        </tr>
+                    </thead>
+                    <tbody className={styles.body}>{needs.length > 0 ? needs.map(needRow) : noDataRow()}</tbody>
+                </table>
             </Box>
-            <table className={styles.table}>
-                <thead>
-                    <tr>
-                        <th className={styles.header}>URGENCY</th>
-                        <th className={styles.header}>ITEMS</th>
-                        <th className={styles.header}>POSTED BY</th>
-                        <th className={styles.header}>COUNTRY</th>
-                        <th className={styles.header}>CITY</th>
-                        <th className={styles.header}>QTY</th>
-                        <th className={styles.header}>DATE</th>
-                        <th className={styles.header}>BUDGET</th>
-                    </tr>
-                </thead>
-                <tbody className={styles.body}>{needs.length > 0 ? needs.map(needRow) : noDataRow()}</tbody>
-            </table>
         </Box>
     );
 }
