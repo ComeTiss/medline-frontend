063d6346e149f54bc73d8534cd1d289535124c65
daveman235 <awad.david@gmail.com>
2020-05-17 14:39:47 -0400

    fixed linter

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -8,7 +8,6 @@ import Footer from "../components/footer/Footer";
 
 import backgroundImage from "../media/person-holding-white-smartphone-on-white-table-3766218.jpg";
 
-
 const useStyles = makeStyles(theme => ({
   card: {
     maxWidth: 600,
@@ -28,11 +27,11 @@ const useStyles = makeStyles(theme => ({
     padding: "40px 0 20px"
   },
   text: {
-    textAlign: "center",
+    textAlign: "center"
   },
   backgroundWrapper: {
     background: `url(${backgroundImage}) no-repeat center center`,
-    backgroundSize: "cover",
+    backgroundSize: "cover"
   },
   container: {
     padding: "1rem"
@@ -75,28 +74,38 @@ const ContactUs = () => {
       });
   };
 
+  const email = (
+    <a href="mailto: support@mediline.io" style={{ textDecoration: "none" }}>
+      support@mediline.io
+    </a>
+  );
+
   return (
     <>
       <NavBar />
       <div className={classes.backgroundWrapper}>
         <div className={classes.container}>
-          <Typography variant="h5" className={`${classes.title} ${classes.text}`}>
+          <Typography
+            variant="h5"
+            className={`${classes.title} ${classes.text}`}
+          >
             Contact us
-        </Typography>
+          </Typography>
           <Typography component="p" className={classes.text}>
-            Have any questions, suggestions, or would like to support MedLine.io?
-        </Typography>
+            Have any questions, suggestions, or would like to support
+            MedLine.io?
+          </Typography>
           <Typography component="p" className={classes.text}>
-            Send us an email to <a href="mailto: support@mediline.io" style={{ textDecoration: 'none' }}>support@mediline.io</a> or fill our this form and
-            we`ll get back to you.
-        </Typography>
+            Send us an email to {email} or fill our this form and we`ll get back
+            to you.
+          </Typography>
           <div className={classes.cardContainer}>
             <Card className={classes.card}>
               {error ? (
                 <>
                   <Typography component="p" className={classes.text}>
                     There was an error, please try again later
-              </Typography>
+                  </Typography>
                   <br />
                   <div className={classes.text}>
                     <Button
@@ -105,52 +114,52 @@ const ContactUs = () => {
                       onClick={() => setError(false)}
                     >
                       Go back to form
-                </Button>
+                    </Button>
                   </div>
                 </>
               ) : (
-                  <form>
-                    <CustomTextField
-                      label="Name"
-                      className={classes.textField}
-                      onChange={handleFieldChange}
-                      name="name"
-                      value={fields.name}
-                    />
-                    <CustomTextField
-                      label="Email (for reply)*"
-                      className={classes.textField}
-                      onChange={handleFieldChange}
-                      name="email"
-                      value={fields.email}
-                    />
-                    <CustomTextField
-                      label="Subject"
-                      className={classes.textField}
-                      onChange={handleFieldChange}
-                      name="subject"
-                      value={fields.subject}
-                    />
-                    <CustomTextField
-                      label="Message*"
-                      className={classes.textField}
-                      onChange={handleFieldChange}
-                      name="message"
-                      value={fields.message}
-                      multiline
-                    />
-                    <div className={classes.buttonWrapper}>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={submit}
-                        disabled={fields.email === "" || fields.message === ""}
-                      >
-                        Submit
-                </Button>
-                    </div>
-                  </form>
-                )}
+                <form>
+                  <CustomTextField
+                    label="Name"
+                    className={classes.textField}
+                    onChange={handleFieldChange}
+                    name="name"
+                    value={fields.name}
+                  />
+                  <CustomTextField
+                    label="Email (for reply)*"
+                    className={classes.textField}
+                    onChange={handleFieldChange}
+                    name="email"
+                    value={fields.email}
+                  />
+                  <CustomTextField
+                    label="Subject"
+                    className={classes.textField}
+                    onChange={handleFieldChange}
+                    name="subject"
+                    value={fields.subject}
+                  />
+                  <CustomTextField
+                    label="Message*"
+                    className={classes.textField}
+                    onChange={handleFieldChange}
+                    name="message"
+                    value={fields.message}
+                    multiline
+                  />
+                  <div className={classes.buttonWrapper}>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={submit}
+                      disabled={fields.email === "" || fields.message === ""}
+                    >
+                      Submit
+                    </Button>
+                  </div>
+                </form>
+              )}
             </Card>
           </div>
         </div>
