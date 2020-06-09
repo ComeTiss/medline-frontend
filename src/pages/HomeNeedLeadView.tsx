1a90a4a70e8b32354caa0bd185c8a3588c1c4819
Frog <shmily40686@gmail.com>
2020-06-08 18:08:20 -0700

    Getting data for table

--- src/pages/HomeNeedLeadView.tsx
+++ src/pages/HomeNeedLeadView.tsx
@@ -84,6 +84,7 @@ const HomeNeedLeadView = () => {
   const needs = needsData?.getAllNeeds?.needs || [];
   const leads = leadsData?.getAllLeads?.leads || [];
 
+
   const styles = useStyles();
 
   const needRow = (need: any) => (
